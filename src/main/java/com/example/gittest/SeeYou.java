ec50d7859b071093dbbe306b047ee692c2e7f708
daizhenwei <1848671814@qq.com>
2021-06-06 08:18:29 +0800

    update .gitignore

---
+++ src/main/java/com/example/gittest/SeeYou.java
@@ -0,0 +1,14 @@
+package com.example.gittest;
+
+/**
+ * 通用测试
+ *
+ * @author 代镇位
+ * @date 2019年03月28日 22:06
+ */
+public class SeeYou {
+    public static void main(String[] args) throws Exception {
+        String aa = "GG";
+        System.out.println(aa);
+    }
+}
